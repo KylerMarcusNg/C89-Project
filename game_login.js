0903d2c1340ed3da7376a90de929a7d211a0a084
KylerMarcusNg <95221449+KylerMarcusNg@users.noreply.github.com>
2022-04-08 19:10:50 +0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  // Write the code to add player 2
+
+    localStorage.setItem("player1_name", player1_name);
+    //write the code to add player2 to local storage
+
+      // Add a code to navigate to game_page.html using window.location
+}
+
